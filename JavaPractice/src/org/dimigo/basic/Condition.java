ebd930cdcdaa78bba86b8a75c858369407f6269c
박준래 <@qkrwnsfo>
2015-03-26 12:09:30 +0900

    네번째 제출과젷ㅎㅎㅎ

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -6,15 +6,26 @@ public class Condition {
 		// TODO Auto-generated method stub
 		int type = 1;
 		int distance = 10;
-		
 		if(type == 1){
 			int flag1 = ((distance-1)/10) * 300 + 850;
+			if(distance <=0){
+				System.out.println("<<고속도로 통행료 계산>>");
+				System.out.println("거리 : "+distance +" km");
+				System.out.println("차종 : 고속버스");
+				System.out.println("통행료 : 0원");
+			}
 			System.out.println("<<고속도로 통행료 계산>>");
 			System.out.println("거리 : "+distance +" km");
 			System.out.println("차종 : 고속버스");
 			System.out.println("통행료 : "+flag1+"원");
 		}
 		else if(type == 2){
+			if(distance <=0){
+				System.out.println("<<고속도로 통행료 계산>>");
+				System.out.println("거리 : "+distance +" km");
+				System.out.println("차종 : 경차");
+				System.out.println("통행료 : 0원");
+			}
 			int flag2 = ((distance-1)/10) * 200 + 300;
 			System.out.println("<<고속도로 통행료 계산>>");
 			System.out.println("거리 : "+distance);
@@ -22,10 +33,16 @@ public class Condition {
 			System.out.println("통행료 : "+flag2+"원");
 		}
 		else {
+			if(distance <=0){
+				System.out.println("<<고속도로 통행료 계산>>");
+				System.out.println("거리 : "+distance +" km");
+				System.out.println("차종 : 그 외");
+				System.out.println("통행료 : 0원");
+			}
 			int flag3 = ((distance-1)/10) * 200 + 600;
 			System.out.println("<<고속도로 통행료 계산>>");
 			System.out.println("거리 : "+distance);
-			System.out.println("차종 : 경차");
+			System.out.println("차종 : 그 외");
 			System.out.println("통행료 : "+flag3+"원");
 		}
 	}
