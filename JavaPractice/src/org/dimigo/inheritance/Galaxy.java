fc1a7a4793e86c4651838e6eb85b0654d07d0c45
박준래 <@qkrwnsfo>
2015-08-25 10:40:58 +0900

    Galaxy VS IPhone

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * |_Galaxy
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 8. 25.
+ * </pre> 
+ *
+ * @auther : 박준래
+ * @version : 1.8
+ */
+public class Galaxy extends SmartPhone {
+	public Galaxy(String model, String company, int price){
+		super(model, company, price);
+	}
+	
+	public void pay(){
+		System.out.println("삼성 페이로 결제합니다.");
+	}
+	
+	public void useWirelessCharging(){
+		System.out.println("무선 충전 기능을 이용합니다.");
+	}
+
+}
