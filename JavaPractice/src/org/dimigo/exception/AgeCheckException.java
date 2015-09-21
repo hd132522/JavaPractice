49099ee94630b8ab0c0b1a654fc6edd495a0116e
박준래 <@qkrwnsfo>
2015-09-21 12:47:36 +0900

    영화보고 싶어요

--- JavaPractice/src/org/dimigo/exception/AgeCheckException.java
+++ JavaPractice/src/org/dimigo/exception/AgeCheckException.java
@@ -15,7 +15,9 @@ package org.dimigo.exception;
  * @version : 1.8
  */
 public class AgeCheckException extends Exception{
-	public AgeCheckException(){};
+	public AgeCheckException(){
+		super();
+	};
 	
 	public AgeCheckException(Movie movie){
 		super(movie.getTitle() +"은/는 " + movie.getLimitAge() +"세 이상 관람가입니다.");
